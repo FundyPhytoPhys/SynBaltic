6007775f06bddd9dd582355e67a299e7751d998b
Your Namesliwins <you@example.coms>
2025-03-29 21:13:20 -0300

    Ulva manuscript

--- Ulva/Manuscript/Manuscript/README.md
+++
@@ -1,5 +0,0 @@
-# Documents
-
-
-- The Docs folder contains 3 fies: BalticPhotoperiod.bib, INSTRUCTIONS.md, and README.md.
-
--- Ulva/Manuscript/Manuscript/custom_filter.lua
+++
@@ -1,7 +0,0 @@
--- custom_filter.lua
-function Image (fig)
-  fig.caption[1] = pandoc.Strong(fig.caption[1])
-  fig.caption[3] = pandoc.Strong(fig.caption[3])
-  fig.caption[4] = pandoc.Strong(".  ")
-  return fig
-end
